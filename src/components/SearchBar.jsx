11e26faaeb6e254dbea124c57644cb5ff5c0a5f5
Sankalan7 <sankalan.chanda312@gmail.com>
2023-02-05 01:19:33 +0530

    small change in SearchBar regarding API key

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,22 +4,24 @@ import "../App.css";
 import Loading from "./Loading";
 
 const API_URL = "https://newsapi.org/v2/everything";
-const apiKey = "58a6e0b58a6243b8900dc02ef32c8d03";
+
+const apiKey_SA = "58a6e0b58a6243b8900dc02ef32c8d03";
+const apiKey_SG = "4eaa5190ac7844a787e3173728a3ae0f";
 
 const SearchBar = ({ setNews1, searchTerm1, setSearchTerm1 }) => {
   // const [loading, setLoading] = useState(true);
   const searchNews = async (title) => {
     const response = await fetch(
-      `${API_URL}?q=${title}&sortBy=popularity&apiKey=${apiKey}`
+      `${API_URL}?q=${title}&sortBy=popularity&apiKey=${apiKey_SG}`
     );
     const data = await response.json();
     setNews1(data.articles);
     // setLoading(false);
     console.log(data.articles);
   };
-  const enterToSearch=(key)=>{
-    if(key.code=="Enter"){
-      searchNews(searchTerm1)
+  const enterToSearch = (key) => {
+    if (key.code == "Enter") {
+      searchNews(searchTerm1);
     }
   };
   // if (loading) {
@@ -35,7 +37,10 @@ const SearchBar = ({ setNews1, searchTerm1, setSearchTerm1 }) => {
         onChange={(e) => setSearchTerm1(e.target.value)}
         onKeyDown={enterToSearch}
       />
-      <FaSearch className=" cursor-pointer h-5 w-5 text-[#a1a1a1]" onClick={() => searchNews(searchTerm1)} />
+      <FaSearch
+        className=" cursor-pointer h-5 w-5 text-[#a1a1a1]"
+        onClick={() => searchNews(searchTerm1)}
+      />
     </div>
   );
 };
