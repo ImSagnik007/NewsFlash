c943af35b542eb18d833e197d7354379c1785be5
sagnik mitra <sagnik.mitra@sagnik.mitra-MacBookPro>
2023-02-04 21:01:46 +0530

    Enter to search enabled

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -17,20 +17,25 @@ const SearchBar = ({ setNews1, searchTerm1, setSearchTerm1 }) => {
     // setLoading(false);
     console.log(data.articles);
   };
-
+  const enterToSearch=(key)=>{
+    if(key.code=="Enter"){
+      searchNews(searchTerm1)
+    }
+  };
   // if (loading) {
   //     return <Loading />;
   //   }
 
   return (
-    <div className="search">
+    <div className="search h-16">
       <input
         className="flex-1 outline-none "
         placeholder="Search for any news"
         value={searchTerm1}
         onChange={(e) => setSearchTerm1(e.target.value)}
+        onKeyDown={enterToSearch}
       />
-      <FaSearch onClick={() => searchNews(searchTerm1)} />
+      <FaSearch className=" cursor-pointer h-5 w-5 text-[#a1a1a1]" onClick={() => searchNews(searchTerm1)} />
     </div>
   );
 };
