977ce8b5d5a8704e3abb73e540ad5d51d469b259
Sankalan7 <sankalan.chanda312@gmail.com>
2023-02-11 17:09:17 +0530

    changed football news card

--- src/components/SideBarComponents/Sports/FootballNewsCard.jsx
+++ src/components/SideBarComponents/Sports/FootballNewsCard.jsx
@@ -9,7 +9,7 @@ const FootballNewsCard = ({ news1 }) => {
     <>
       <div className="w-full mt-2 mb-2 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-500 ">
         <a href="#">
-          <img className="rounded-t-lg w-full" src={news1.thumbnailUrl} alt="" />
+          <img className="rounded-t-lg" src={news1.thumbnailUrl} alt="" />
         </a>
         <div className="p-5">
           <a href="#">
@@ -20,7 +20,7 @@ const FootballNewsCard = ({ news1 }) => {
           <p className="mb-3 font-normal text-gray-700 dark:text-gray-400"></p>
           <a
             onClick={handleClick}
-            className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-red-800 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 cursor-pointer"
+            className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 cursor-pointer"
           >
             Watch it on YouTube
           </a>
